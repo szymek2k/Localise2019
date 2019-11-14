364182acf3483ae83b5c2d1f772fa9b0999aa37d
MarcinSz <marcin@o2.pl>
2019-11-14 20:36:50 +0100

    feat: Configure Webpack for SASS and CSS

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".jsx", ".ts", ".tsx", ".js"]
   },
   entry: {
-    index: './src/index.tsx',
+    index: "./src/index.tsx"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -28,6 +27,16 @@ module.exports = {
           loader: "babel-loader"
         }
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
@@ -40,18 +49,17 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
+      chunks: ["index"],
+      filename: "index.html"
     })
-  ],
+  ]
 };
 
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //   react: "React",
-  //   "react-dom": "ReactDOM"
-  // }
+// When importing a module whose path matches one of the following, just
+// assume a corresponding global variable exists and use that instead.
+// This is important because it allows us to avoid bundling all of our
+// dependencies, which allows browsers to cache those libraries between builds.
+// externals: {
+//   react: "React",
+//   "react-dom": "ReactDOM"
+// }
